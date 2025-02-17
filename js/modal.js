c2360796f9c76411b822f7ba0c161d8c76a1280b
DoubleDevT <vetminecrafter@gmail.com>
2025-02-17 11:00:08 +0200

    add mob styles for header, hero and benefits sections

--- js/modal.js
+++ js/modal.js
@@ -6,13 +6,26 @@
         closeModalBtn: document.querySelector("[data-modal-close]"),
         // Додати атрибут data-modal на бекдроп модалки
         modal: document.querySelector("[data-modal]"),
+
+        openMobMenuBtn: document.querySelector("[data-mob-menu-open]"),
+
+        closeMobMenuBtn: document.querySelector("[data-mob-menu-close]"),
+
+        MobMenu: document.querySelector("[data-mob-menu]"),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
 
+    refs.openMobMenuBtn.addEventListener("click", toggleMobMenu);
+    refs.closeMobMenuBtn.addEventListener("click", toggleMobMenu);
+
     function toggleModal() {
         // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
         refs.modal.classList.toggle("is-open");
+    }
+    function toggleMobMenu() {
+        // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+        refs.MobMenu.classList.toggle("is-open");
     }
 })();
